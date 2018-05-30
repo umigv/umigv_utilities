f900e45371a23874ed917b14040eb15eaed286e4
Gregory Meyer <gregjm@umich.edu>
2018-05-29 20:11:18 -0400

    restructure, change to use invoke/fold backend

--- include/umigv_utilities/zip.hpp
+++ include/umigv_utilities/zip.hpp
@@ -3,102 +3,144 @@
 
 // defines umigv::zip, which takes a variadic number of iterable ranges and zips them together
 
+#include "umigv_utilities/fold.hpp"
+#include "umigv_utilities/traits.hpp"
 #include "umigv_utilities/types.hpp"
-#include "umigv_utilities/detail/boolean.hpp"
 
-#include <type_traits>
 #include <iterator>
-#include <tuple> // std::tuple, std::tuple_element_t
+#include <memory>
+#include <tuple>
+#include <type_traits>
+
+#include <boost/optional.hpp>
 
 namespace umigv {
 
-template <typename IteratorTuple, usize ...Is>
+template <typename IterTuple, usize ...Is>
+class ZippedRange;
+
+template <typename Iterators, usize ...Is>
+class ZippedRangeIterator {
+public:
+    friend ZippedRange<Iterators, Is...>;
+
+    using difference_type = isize;
+    using value_type = std::tuple<iterator_reference_t<
+        std::tuple_element_t<Is, Iterators>
+    >...>;
+    using pointer = const value_type*;
+    using reference = const value_type&;
+    using iterator_category = std::input_iterator_tag;
+
+    constexpr reference operator*() const {
+        if (any_equal_last()) {
+            throw std::out_of_range{ "ZippedRangeIterator::operator*" };
+        }
+
+        deref_.emplace(*std::get<Is>(current_)...);
+
+        return deref_.value();
+    }
+
+    constexpr pointer operator->() const {
+        return std::addressof(**this);
+    }
+
+    constexpr ZippedRangeIterator& operator++() {
+        if (any_equal_last()) {
+            throw std::out_of_range{ "ZippedRangeIterator::operator++" };
+        }
+
+        auto increment_op = [](auto &&x) {
+            return ++std::forward<decltype(x)>(x);
+        };
+
+        unary_fold(increment_op, std::get<Is>(current_)...);
+
+        return *this;
+    }
+
+    constexpr friend bool operator==(const ZippedRangeIterator lhs,
+                                     const ZippedRangeIterator rhs) {
+        if (!boolean_and(
+            (std::get<Is>(lhs.last_) == std::get<Is>(rhs.last_))...
+        )) {
+            throw std::out_of_range{
+                "operator==(ZippedRangeIterator, ZippedRangeIterator)"
+            };
+        }
+
+        return boolean_or(
+            (std::get<Is>(lhs.current_) == std::get<Is>(rhs.current_))...
+        );
+    }
+
+    constexpr friend bool operator!=(const ZippedRangeIterator lhs,
+                                     const ZippedRangeIterator rhs) {
+        if (!boolean_and(
+            (std::get<Is>(lhs.last_) == std::get<Is>(rhs.last_))...
+        )) {
+            throw std::out_of_range{
+                "operator!=(ZippedRangeIterator, ZippedRangeIterator)"
+            };
+        }
+
+        return boolean_and(
+            (std::get<Is>(lhs.current_) != std::get<Is>(rhs.current_))...
+        );
+    }
+
+private:
+    constexpr ZippedRangeIterator(Iterators current, Iterators last)
+    noexcept(std::is_nothrow_move_constructible<Iterators>::value)
+    : current_{ std::move(current) }, last_{ std::move(last) } { }
+
+    constexpr bool any_equal_last() const
+    noexcept(conjunction_v<is_nothrow_equality_comparable<
+        std::tuple_element_t<Is, value_type>
+    >...>) {
+        const auto equal_op = [](auto &&lhs, auto &&rhs) -> decltype(auto) {
+            return std::forward<decltype(lhs)>(lhs)
+                   == std::forward<decltype(rhs)>(rhs);
+        };
+
+        const auto or_op = [](auto &&lhs, auto &&rhs) -> decltype(auto) {
+            return std::forward<decltype(lhs)>(lhs)
+                   || std::forward<decltype(rhs)>(rhs);
+        };
+
+        return binary_fold_pairs(or_op, equal_op, current_, last_);
+    }
+
+    Iterators current_;
+    Iterators last_;
+    mutable boost::optional<value_type> deref_;
+};
+
+template <typename Iterators, usize ...Is>
 class ZippedRange {
     template <typename T>
     using DereferenceT = decltype((*std::declval<T>()));
 
 public:
-    class Iterator {
-    public:
-        friend ZippedRange;
+    using iterator = ZippedRangeIterator<Iterators, Is...>;
+    using difference_type = iterator_difference_type<iterator>;
 
-        using difference_type = isize;
-        using value_type = std::tuple<DereferenceT<
-            std::tuple_element_t<Is, IteratorTuple>
-        >...>;
-        using pointer = const value_type*;
-        using reference = const value_type&;
-        using iterator_category = std::forward_iterator_tag;
+    constexpr ZippedRange(Iterators first, Iterators last)
+    noexcept(std::is_nothrow_move_constructible<Iterators>::value)
+    : first_{ std::move(first) }, last_{ std::move(last) } { }
 
-        Iterator& operator++() {
-            __attribute__((unused)) auto i =
-                { (++std::get<Is>(current_), 0)... };
-
-            return *this;
-        }
-
-        value_type operator*() const {
-            return value_type{ *std::get<Is>(current_)... };
-        }
-
-        friend bool operator==(const Iterator lhs, const Iterator rhs)
-            noexcept(noexcept(
-                detail::boolean_or((std::get<Is>(std::declval<IteratorTuple>())
-                                    == std::get<Is>(
-                                        std::declval<IteratorTuple>()
-                                    ))...)
-            ))
-        {
-            return detail::boolean_or((std::get<Is>(lhs.current_)
-                                       == std::get<Is>(rhs.current_))...);
-        }
-
-        friend bool operator!=(const Iterator lhs, const Iterator rhs)
-            noexcept(noexcept(
-                detail::boolean_and((std::get<Is>(std::declval<IteratorTuple>())
-                                     != std::get<Is>(
-                                         std::declval<IteratorTuple>()
-                                     ))...)
-            ))
-        {
-            return detail::boolean_and((std::get<Is>(lhs.current_)
-                                        != std::get<Is>(rhs.current_))...);
-        }
-
-    private:
-        explicit Iterator(IteratorTuple current)
-            noexcept(noexcept(
-                IteratorTuple{ std::move(std::declval<IteratorTuple>()) }
-            ))
-            : current_{ std::move(current) }
-        { }
-
-        IteratorTuple current_;
-    };
-
-    ZippedRange(IteratorTuple begins, IteratorTuple ends)
-        noexcept(noexcept(
-            IteratorTuple{ std::move(std::declval<IteratorTuple>()) }
-        ))
-        : begins_{ std::move(begins) },
-          ends_{ std::move(ends) }
-    { }
-
-    Iterator begin() const
-        noexcept(noexcept(Iterator{ std::declval<IteratorTuple>() }))
-    {
-        return Iterator{ begins_ };
+    constexpr iterator begin() const {
+        return { first_, last_ };
     }
 
-    Iterator end() const
-        noexcept(noexcept(Iterator{ std::declval<IteratorTuple>() }))
-    {
-        return Iterator{ ends_ };
+    constexpr iterator end() const {
+        return { last_, last_ };
     }
 
 private:
-    IteratorTuple begins_;
-    IteratorTuple ends_;
+    Iterators first_;
+    Iterators last_;
 };
 
 template <typename IteratorTuple, usize ...Is>
