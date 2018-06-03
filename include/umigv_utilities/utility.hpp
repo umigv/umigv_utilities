337490593997a0caa42c37516dc7a6173249ef5e
Gregory Meyer <gregjm@umich.edu>
2018-06-03 11:49:29 -0400

    rename ::adl::* -> ::umigv::adl::adl_*

--- include/umigv_utilities/utility.hpp
+++ include/umigv_utilities/utility.hpp
@@ -28,33 +28,32 @@ constexpr const To& byte_cast(const From &from) noexcept {
     return *reinterpret_cast<const To*>(&from);
 }
 
-} // namespace umigv
-
-namespace adl {
+inline namespace adl {
 
 using std::swap;
 using std::begin;
 using std::end;
 
 template <typename T, std::enable_if_t<umigv::has_begin_v<T>, int> = 0>
-constexpr umigv::begin_result_t<T> begin(T &&t)
+constexpr umigv::begin_result_t<T> adl_begin(T &&t)
 noexcept(umigv::has_nothrow_begin_v<T>) {
     return begin(std::forward<T>(t));
 }
 
 template <typename T, std::enable_if_t<umigv::has_end_v<T>, int> = 0>
-constexpr umigv::end_result_t<T> end(T &&t)
+constexpr umigv::end_result_t<T> adl_end(T &&t)
 noexcept(umigv::has_nothrow_end_v<T>) {
     return end(std::forward<T>(t));
 }
 
 template <typename T, typename U,
           std::enable_if_t<umigv::is_swappable_with_v<T, U>, int> = 0>
-constexpr void swap(T &&t, U &&u)
+constexpr void adl_swap(T &&t, U &&u)
 noexcept(umigv::is_nothrow_swappable_with_v<T, U>) {
     return swap(std::forward<T>(t), std::forward<U>(u));
 }
 
 } // namespace adl
+} // namespace umigv
 
 #endif
